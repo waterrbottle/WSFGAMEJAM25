497e50679df1f3c1fba0a3fd8054b1c759d238e2
waterrbottle <112860891+waterrbottle@users.noreply.github.com>
2025-11-08 01:32:50 +0100

    FG
    FH

--- gamej-amwsf-25/objcts/conductor.gd
+++ gamej-amwsf-25/objcts/conductor.gd
@@ -1,6 +1,6 @@
 extends Node3D
 var direction = 1
-
+var go := true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,8 +8,26 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += delta *3* direction
-	if position.x > 15:
-		direction = -1
-	if position.x < -15:
-		direction = 1
+	if go == true:
+		position.x += delta *3* direction
+
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://objcts/dedscreen.tscn")
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.is_in_group("dirswitch"):
+		direction *= -1
+	if area.is_in_group("enterdetector"):
+		if randi_range(1,5) == 1:
+			go = false
+			position.z -= 2
+			$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	go = true
+	position.z += 2
