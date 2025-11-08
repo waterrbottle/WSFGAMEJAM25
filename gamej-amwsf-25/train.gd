a6a1524bcf98170fd8a5270e97bf67187f1c34e7
waterrbottle <112860891+waterrbottle@users.noreply.github.com>
2025-11-08 13:31:56 +0100

    sd
    d

--- gamej-amwsf-25/train.gd
+++ gamej-amwsf-25/train.gd
@@ -13,3 +13,9 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	$Camera3D/AnimationPlayer.play("finish")
+	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "finish":
+		get_tree().change_scene_to_file("res://objcts/trainscene.tscn")
